159acb0b67250545c77f289d31d373b3da45b0e2
Victor Barbosa <freitasdfb@gmail.com>
2019-03-18 17:35:00 -0300

    Commit base

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    username = 'root',
+    password = 'pix2000',
+    database = 'snippetapp',
+    host = '127.0.0.1',
+    dialect = 'mysql',
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path')
+const nunjucks = require('nunjucks')
+
+const app = express();
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+nunjucks.configure(path.resolve('app','views'), {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine','njk');
+
+app.get('/' , (req, res) => {
+    res.render('index')
+});
+
+app.listen(3000);
