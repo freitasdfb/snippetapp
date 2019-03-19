62cb60c0fe2b7553bf33713f9076d4b2ede5c206
Victor Barbosa <freitasdfb@gmail.com>
2019-03-19 15:27:30 -0300

    Correções de identação

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path')
-const nunjucks = require('nunjucks')
+const bodyParser = require('body-parser');
+const path = require('path');
+const nunjucks = require('nunjucks');
 
 const app = express();
-
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 nunjucks.configure(path.resolve('app','views'), {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app,
 });
 
-app.set('view engine','njk');
+app.set('view engine', 'njk');
 
-app.get('/' , (req, res) => {
-    res.render('index')
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(3000);
